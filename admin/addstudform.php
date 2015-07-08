d98a3805276fcc493cba913ea43c76ff74e138a0
= <=>
2015-07-09 05:29:19 +0800

    Added student adding form

--- admin/addstudform.php
+++ admin/addstudform.php
@@ -1,18 +1,21 @@
-<div id="" class="">
+<div id="add_student_form" class="">
 
 	<!-- Display for the message result of adding student -->
-
+	<div id="msg_add_stud"></div>
+	
 	<h2>Add Registered Students</h2>
-	
-	Student Number:
-	<input class="text-uppercase form-control" type="text" maxlength="11" name="stdntnum" id="stdntnum"/> 
-	First Name:
-	<input class="text-uppercase form-control" type="text" maxlength="30" name="fname" id="fname"/>
-	Last Name:
-	<input class="text-uppercase form-control" type="text" maxlength="30" name="lname" id="lname"/>
-	PIN:
-	<input class="text-uppercase form-control" type="text" maxlength="4" name="pin" id="pin"/>
-	
-	<br/>
-	<button class="btn btn-success">Add Student</button>
+	<form>
+		Student Number:
+		<input class="text-uppercase form-control" type="text" maxlength="11" name="stdntnum" id="stdntnum" required/> 
+		First Name:
+		<input class="text-capitalized form-control" type="text" maxlength="30" name="fname" id="fname" required />
+		Last Name:
+		<input class="text-capitalized form-control" type="text" maxlength="30" name="lname" id="lname" required/>
+		PIN:
+		<input class="text-uppercase form-control" type="text" maxlength="4" name="pin" id="pin" required/>
+		<br/>
+		<input class="btn btn-danger" type="reset" value="Clear All"/>
+		<br/><br/>
+	</form>
+	<button class="btn btn-success" onclick="">Add Student</button> <!-- Add js function to call onclick -->
 </div>
